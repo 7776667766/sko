90a917b1f85779076d3f735fd6897ff389a4a980
Usama Subhani <reachusama@protonmail.com>
2020-07-02 00:01:55 +0500

    Show products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,32 @@
 import React, { useContext } from 'react';
+import { Card, Button } from 'react-bootstrap'
 // import { Card, Typography, CardContent, Button } from '@material-ui/core'
 
-// import { GlobalContext } from '../context/GlobalContext'
+import { GlobalContext } from '../context/GlobalContext'
 
 const Product = ({ product }) => {
-    // const { cart, addToCart, increase } = useContext(GlobalContext);
+    const { cart, addToCart, increase } = useContext(GlobalContext);
 
-    // const add = () => {
-    //     if (cart.find(item => item.id === product.id)) {
-    //         // product.quantity++;
-    //         increase(product.id)
-    //     } else {
-    //         product.quantity=1;
-    //         addToCart({product});
-    //     }
-    // }
+    const add = () => {
+        if (cart.find(item => item.id === product.id)) {
+            // product.quantity++;
+            increase(product.id)
+        } else {
+            product.quantity=1;
+            addToCart({product});
+        }
+    }
 
     return (
-        <div></div>
+        <div className="product">
+            <Card border="light">
+            {/* <img src={product.img} /> */}
+            <Card.Img variant="top" src={product.img} />
+            <Card.ImgOverlay>
+                {/* <Card.Title>{product.name}</Card.Title> */}
+            </Card.ImgOverlay>
+            </Card>
+        </div>
         // <div className="product">
         //     <Card variant="outlined">
         //             <CardContent>
--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,36 +1,40 @@
 import React, { useContext } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-// import './productGrid.css'
+import './productGrid.css'
 import Product from './Product'
-// import { GlobalContext } from '../context/GlobalContext'
+import { GlobalContext } from '../context/GlobalContext'
 
 const ProductGrid = () => {
-    // const { products } = useContext(GlobalContext);
+    const { products } = useContext(GlobalContext);
     
     return (
-        <div>
-        <Container>
-            <Row>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            </Row>
-            <Row>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            </Row>
-            <Row>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            <Col md><h1>Product</h1></Col>
-            </Row>
-        </Container>
-        </div>
-        // <div className="productGrid">
-            
-        //     {products.map(product => (<Product key={product.id} product={product} />))}
+        // <div>
+        // <Container>
+
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        //     <Col sm><h1>Product</h1></Col>
+        // </Container>
         // </div>
+        <div className="productGrid justify-content-center">
+            
+            {products.map(product => (<Product key={product.id} product={product} />))}
+        </div>
     )
 }
 
