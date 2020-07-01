59700248648169cc4e016016b3d53d77dbb72bdd
Usama Subhani <reachusama@protonmail.com>
2020-07-01 21:20:36 +0500

    Setup router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
+import { Nav, Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      Shoe Store
-    </div>
+    <Router>
+      <Nav className="justify-content-center" activeKey="/">
+        <Nav.Item>
+          <Nav.Link><Link to="/">Home</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+        <Nav.Link as="Link"><Link to="/cart">Cart</Link></Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
+}
+function Home() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col md>Product</Col>
+          <Col md>Product</Col>
+          <Col md>Product</Col>
+        </Row>
+        <Row>
+          <Col md>Product</Col>
+          <Col md>Product</Col>
+          <Col md>Product</Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  )
 }
 
 export default App;
